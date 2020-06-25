3ab119128a3ada276486275c2717f51a2263218a
Ahmad <albastov.ah@gmail.com>
2020-06-25 20:14:37 +0400

    init commit

---
+++ src/main/java/lesson1/hw1/task3/Orange.java
@@ -0,0 +1,10 @@
+package lesson1.hw1.task3;
+
+public class Orange extends Fruit {
+
+   float orangeWeight;
+
+    public Orange(float orangeWeight) {
+        this.orangeWeight = orangeWeight;
+    }
+}
